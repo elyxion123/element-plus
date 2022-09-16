a04202221267229763efad8d5093493105d942e8
elyxion123 <106503191+elyxion123@users.noreply.github.com>
2022-09-16 17:00:03 +0800

    Update example.vue

--- docs/examples/container/example.vue
+++ docs/examples/container/example.vue
@@ -8,11 +8,9 @@
               <el-icon><message /></el-icon>Navigator One
             </template>
             <el-menu-item-group>
-              <template #title>Group 1</template>
               <el-menu-item index="1-1">Option 1</el-menu-item>
               <el-menu-item index="1-2">Option 2</el-menu-item>
             </el-menu-item-group>
-            <el-menu-item-group title="Group 2">
               <el-menu-item index="1-3">Option 3</el-menu-item>
             </el-menu-item-group>
             <el-sub-menu index="1-4">
